cbf687163846556f00f67cbd8f1f07aa3196e2b1
Russell Kvashnin <russell.kvashnin@gmail.com>
2023-04-16 23:25:34 +0300

    install dir stuff

--- installer.sh
+++ installer.sh
@@ -6,9 +6,17 @@ set -euo pipefail
 
 EXECUTABLE_NAME="golaya"
 EXECUTABLE_TMP_NAME="$EXECUTABLE_NAME-"$(date +"%s")
-EXECUTABLE_DIR="${HOME}/.local/bin"
+EXECUTABLE_DIR=""
+DEFAULT_EXECUTABLE_DIR="${HOME}/.golaya/bin"
+
+USER_BIN_DIRS=(
+  "${HOME}"/bin
+  "${HOME}"/.local/bin
+)
+
+IS_USR_DIR_IN_PATH=true
+
 DOWNLOAD_URL="https://gist.githubusercontent.com/russell-kvashnin/acd9d71823dc36d1ee7670a9108a6751/raw/c47dd62cbe3b393cbb80f40fcef0000a1ae5e17d/golaya.sh"
-EXECUTABLE_DEST="${EXECUTABLE_DIR}/${EXECUTABLE_NAME}"
 
 downloader=""
 
@@ -60,13 +68,39 @@ function download {
   echo "Done."
 }
 
+function prepare_install_dir {
+  for usrBinDir in "${USER_BIN_DIRS[@]}"; do
+    if [[ ":$PATH:" == *":${usrBinDir}:"* && -d "${usrBinDir}" ]]; then
+        echo "User executables directory found and selected for installation: ${usrBinDir}"
+
+        EXECUTABLE_DIR=$usrBinDir
+
+        break
+    fi
+  done
+
+  if [[ "${EXECUTABLE_DIR}" == "" ]]; then
+    echo "Failed to find default user binary directory. Proceed with ${DEFAULT_EXECUTABLE_DIR}"
+
+    EXECUTABLE_DIR=$DEFAULT_EXECUTABLE_DIR
+    IS_USR_DIR_IN_PATH=false
+
+    if [ ! -d "${EXECUTABLE_DIR}" ]; then
+      echo "Directory ${EXECUTABLE_DIR} is missing, creating."
+
+      mkdir -p "$EXECUTABLE_DIR"
+      exit_if_error $? "Failed to create ${EXECUTABLE_DIR}. Exiting"
+    fi
+  fi
+}
+
 function install_executable {
   echo "Installing executable into $EXECUTABLE_DIR ..."
 
-  mv "/tmp/${EXECUTABLE_TMP_NAME}" "${EXECUTABLE_DEST}"
+  mv "/tmp/${EXECUTABLE_TMP_NAME}" "${EXECUTABLE_DIR}"/"${EXECUTABLE_NAME}"
   exit_if_error $? "Failed to move executable. Exiting."
 
-  chmod +x "${EXECUTABLE_DEST}"
+  chmod +x "${EXECUTABLE_DIR}"/"${EXECUTABLE_NAME}"
   exit_if_error $? "Failed to make script executable. Exiting."
 
   echo "Done."
@@ -78,6 +112,19 @@ function exit_if_error {
 
     exit 1
   fi
+}
+
+function display_post_install_message {
+  echo "-----------------------------------------------------------------"
+  echo "Installation succesfull."
+  echo "Executable is on ${EXECUTABLE_DIR}/${EXECUTABLE_NAME}"
+
+  if ! "${IS_USR_DIR_IN_PATH}"; then
+    echo "Do not forget: export PATH=${EXECUTABLE_DIR}:\$PATH"
+  fi
+
+  echo "Enjoy."
+  echo "-----------------------------------------------------------------"
 }
 
 #######################################
@@ -94,9 +141,14 @@ function main {
 
   download
 
+  prepare_install_dir
+
   install_executable
 
-  echo "Installation successful."
+  display_post_install_message
+
+  exit 0
 }
 
 main
+
